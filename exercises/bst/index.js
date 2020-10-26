d574b1351f691b7eb28b2dd20db4c3b9653f31bc
gillyspy <gillyspy@gmail.com>
2020-10-25 23:14:02 -0400

    b tree and validation

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,43 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        [this.left, this.right] = [null, null]
+        this.data = data
+
+    }
+
+    insert(data) {
+        let node = this;
+        if (data < this.data) {
+            if (this.left === null) {
+                this.left = new Node(data)
+
+            } else {
+                this.left.insert(data)
+            }
+        } else { // data > this.data
+            if (this.right === null) {
+                this.right = new Node(data)
+            } else {
+                this.right.insert(data)
+            }
+        }
+        return
+    }
+
+    contains(data) {
+        let node = this;
+        if (data === this.data) {
+            return this
+        } else if (data < this.data && this.left) {
+            return this.left.contains(data)
+        } else if (data > this.data && this.right) {
+            return this.right.contains(data)
+        }
+        return null
+    }
+}
 
 module.exports = Node;
--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,46 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+    let isValid
+    //move left, then right
+    /*
+    traversing down:
+    -if there is a left in question go left.
+    -if there is not a left in question then go right
+    -if there is anot a right then we're good
+     */
+    min = node.left ? node.left.data : min
+    max = node.right ? node.right.data : max
+
+    if (//
+        (
+            node.left &&
+            node.data < node.left.data &&
+            node.left.data >= min
+        ) ||
+        (
+            node.right &&
+            node.data > node.right.data &&
+            node.right.data <= max
+        )
+    ) {
+        return false
+    }
+
+    isValid = true
+    /*if (!node.left && !node.right) {
+        return isValid
+    }*/
+
+    if (node.left && isValid) {
+        isValid = validate(node.left, node.left.data, max)
+    }
+
+    if (node.right && isValid) {
+        isValid = validate(node.right, min, node.right.data)
+    }
+    return isValid
+}
 
 module.exports = validate;
